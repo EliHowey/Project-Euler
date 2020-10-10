2c2e8a25264e6db130cc5ca777326cf1fb360bf2
Eli Howey <elihowey@gmail.com>
2020-10-10 13:38:53 -0400

    feat: :sparkles: add solution to Problem 2

---
+++ tests/002.test.js
@@ -0,0 +1,22 @@
+import { describe, expect, test } from '@jest/globals';
+import { problem002v1, problem002v2 } from '../solutions/002';
+
+describe('v1', () => {
+	test('sums even Fibonacci numbers below 10', () => {
+		expect(problem002v1(10)).toBe(10);
+	});
+
+	test('sums even Fibonacci numbers below 4000000', () => {
+		expect(problem002v1(4000000)).toBe(4613732);
+	});
+});
+
+describe('v2', () => {
+	test('sums even Fibonacci numbers below 10', () => {
+		expect(problem002v2(10)).toBe(10);
+	});
+
+	test('sums even Fibonacci numbers below 4000000', () => {
+		expect(problem002v2(4000000)).toBe(4613732);
+	});
+});
